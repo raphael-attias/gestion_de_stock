45d2d31d8f7f51cd3f999e6191979feb1fabb009
raphael-attias <raphael.attias@laplateforme.io>
2024-02-02 13:19:10 +0100

    interaction Bd + Export csv

--- backend.py
+++ backend.py
@@ -12,7 +12,26 @@ class StockManager:
 
     def __del__(self):
         self.connection.close()
+    
+    def get_category_id_by_name(self, category_name):
+        query = "SELECT id FROM category WHERE name = %s"
+        self.cursor.execute(query, (category_name,))
+        result = self.cursor.fetchone()
+        if result:
+            return result[0]
+        else:
+            return None
 
+    def get_categories(self):
+        self.cursor.execute("SELECT * FROM category")
+        return self.cursor.fetchall()
+
+    def get_category_name_by_id(self, category_id):
+        query = "SELECT name FROM category WHERE id = %s"
+        self.cursor.execute(query, (category_id,))
+        result = self.cursor.fetchone()
+        return result[0] if result else None
+    
     def get_products(self):
         self.cursor.execute("SELECT * FROM product")
         return self.cursor.fetchall()
@@ -21,6 +40,11 @@ class StockManager:
         query = "INSERT INTO product (name, description, price, quantity, id_category) VALUES (%s, %s, %s, %s, %s)"
         values = (name, description, price, quantity, category_id)
         self.cursor.execute(query, values)
+        self.connection.commit()
+    
+    def add_category(self, name):
+        query = "INSERT INTO category (name) VALUES (%s)"
+        self.cursor.execute(query, (name,))
         self.connection.commit()
 
     def delete_product(self, product_id):
